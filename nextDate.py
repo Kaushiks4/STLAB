db06ae905dba318ac715098e03ef25c25ca7afc4
Kaushiks4 <48856232+Kaushiks4@users.noreply.github.com>
2021-01-21 14:20:38 +0530

    Update nextDate.py

--- nextDate.py
+++ nextDate.py
@@ -2,42 +2,25 @@ def nextDate(day,month,year):
     newDay = day
     newMonth = month
     newYear = year
+    switcher = {1 : 31,2 : 28,3 : 31,4 : 30,5 : 31,6 : 30,7 : 30,8 : 31,9 : 30,10 : 31,11 : 30,12 : 31}
     if year % 400 == 0:
-        switcher = {1 : 31,2 : 29,3 : 31,4 : 30,5 : 31,6 : 30,7 : 30,8 : 31,9 : 30,10 : 31,11 : 30,12 : 31}
-        if day > switcher[month]:
-            print("Invalid date")
-            return
-        if day == switcher[month]:
-            newDay = 1
-            if month == 12:
-                newMonth = 1
-                if year == 2014:
-                    print("year exceeded")
-                    return
-                else:
-                    newYear = year + 1
+        switcher[2] = 29
+    if day > switcher[month]:
+        print("Invalid date")
+        return
+    if day == switcher[month]:
+        newDay = 1
+        if month == 12:
+            newMonth = 1
+            if year == 2014:
+                print("year exceeded")
+                return
             else:
-                newMonth = month + 1
+                newYear = year + 1
         else:
-            newDay = day + 1
-    else: 
-        switcher = {1 : 31,2 : 28,3 : 31,4 : 30,5 : 31,6 : 30,7 : 30,8 : 31,9 : 30,10 : 31,11 : 30,12 : 31}
-        if day > switcher[month]:
-            print("Invalid date")
-            return
-        elif day == switcher[month]:
-            newDay = 1
-            if month == 12:
-                newMonth = 1
-                if year == 2048:
-                    print("year exceeded")
-                    return
-                else:
-                    newYear = year + 1
-            else:
-                newMonth = month + 1
-        else:
-            newDay = day + 1
+            newMonth = month + 1
+    else:
+        newDay = day + 1
     print(str(newDay) + '-' + str(newMonth) + '-' + str(newYear))
     return
 
