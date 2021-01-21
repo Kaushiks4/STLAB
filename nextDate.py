25f31fb40b30c355795487b0156081bf395276a7
Kaushiks4 <48856232+Kaushiks4@users.noreply.github.com>
2021-01-21 14:27:56 +0530

    Update nextDate.py

--- nextDate.py
+++ nextDate.py
@@ -2,7 +2,7 @@ def nextDate(day,month,year):
     newDay = day
     newMonth = month
     newYear = year
-    switcher = {1 : 31,2 : 28,3 : 31,4 : 30,5 : 31,6 : 30,7 : 30,8 : 31,9 : 30,10 : 31,11 : 30,12 : 31}
+    switcher = {1 : 31,2 : 28,3 : 31,4 : 30,5 : 31,6 : 30,7 : 31,8 : 31,9 : 30,10 : 31,11 : 30,12 : 31}
     if year % 400 == 0:
         switcher[2] = 29
     if day > switcher[month]:
